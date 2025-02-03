da9614133e5e32f89f82942c8e38572a2af2efbe
user <198586713+solyplaya@users.noreply.github.com>
2025-02-03 19:45:34 +0000

    refactored verbosity leves and message printing. Added --no-color option, --compact and --muted verbosity levels. Version bump to 1.2
    FossilOrigin-Name: 472428eca7d7785c77fb40e773e5a01756d5e59f26321410502d0eae1600f5d0

--- presync.sh
+++ presync.sh
@@ -41,24 +41,36 @@ set -o nounset
 hasher="xxh128sum"
 tmp="/tmp"
 
-VERSION="1.1"
+VERSION="1.2"
+
+readonly SHOW_FROM_DEBUG="5"
+readonly SHOW_FROM_VERBOSE="4"
+readonly SHOW_FROM_NORMAL="3"
+readonly SHOW_FROM_COMPACT="2"
+readonly SHOW_FROM_QUIET="1"
+readonly SHOW_FROM_MUTED="0"
+
+readonly MSG_TYPE_ERROR="error"
+readonly MSG_TYPE_INFO="info"
+readonly MSG_TYPE_WARNING="warning"
+readonly MSG_TYPE_NORMAL="normal"
+readonly MSG_TYPE_INPLACE="inplace"
 
 src=""
 dst=""
 db="${tmp}/presync.sqlite3"
 
-debug=0
 dry_run=0
 flush_db=0
 head_size=1024
 keep_db=0
+no_color=0
 partial=0
 progress=0
-quiet=0
 resume=0
 reuse_db=0
 term_width=80
-verbose=0
+verbosity="$SHOW_FROM_NORMAL"
 
 add_to_db() {
 
@@ -69,7 +81,7 @@ add_to_db() {
     if [[ -n "$hash" ]]; then
         db_query "INSERT OR REPLACE INTO files (hash, path) VALUES ('${hash//\'/\'\'}', '${file//\'/\'\'}');"
     else
-        error_msg "Error: cannot generate checksum for file: $file"
+        msg_error "Error: cannot generate checksum for file: $file"
     fi
 
 }
@@ -96,15 +108,13 @@ collect_target_hashes() {
     local total=0
     local resume_file=""
     local progress_msg=""
+    local percent=""
 
-    echo "Collecting target dir file checksums..."
+    msg "Collecting target dir file checksums..." "$SHOW_FROM_COMPACT"
 
     [[ "$progress" = 1 ]] && total=$(get_total_files "$dst")
-
     [[ "$resume" = 1 ]] && resume_file=$(get_last_file)
 
-    # https://stackoverflow.com/questions/71270045/print-periodic-progress-messages-on-the-same-line
-    # progress bar
     while IFS= read -d '' -r file ; do
 
         (( idx++ ))
@@ -116,9 +126,22 @@ collect_target_hashes() {
             continue
         fi
 
-        [[ "$progress" = 1 ]] && progress_msg="[${idx}/${total}] "
+        if [[ "$progress" = 1 ]]; then
+            percent=$((idx * 100 / total))
+            progress_msg="[${idx}/${total} (${percent}%] "
+        fi
 
-        inplace_msg "${progress_msg}${file}"
+        if [[ "$verbosity" -eq "$SHOW_FROM_QUIET" ]]; then
+            [[ "$progress" = 1 ]] && msg_inplace "Collecting target hashes: ${idx}/${total} (${percent}%)" "$SHOW_FROM_QUIET"
+        else
+
+            if [[ ( "$verbosity" -eq "$SHOW_FROM_COMPACT" || "$verbosity" -eq "$SHOW_FROM_NORMAL" ) ]]; then
+                msg_inplace "${progress_msg}${file}" "$verbosity"
+            else
+                msg_normal "${progress_msg}${file}"
+            fi
+
+        fi
 
         add_to_db "${file}"
 
@@ -150,7 +173,7 @@ db_init() {
         fi
 
         if [[ "$reuse_db" = 1 ]]; then
-            info_msg "Reusing existing database file: $db"
+            msg_info "Reusing existing database file: $db"
             return
         fi
 
@@ -178,17 +201,8 @@ db_query() {
 
 error_exit() {
 
-    error_msg "$1"
+    msg_error "$1"
     exit 1
-
-}
-
-error_msg() {
-
-    local red='\033[1;31m'
-    local nocolor='\033[0m'
-
-    echo -e "\n${red}${1:-}${nocolor}"
 
 }
 
@@ -245,42 +259,6 @@ get_total_files() {
 
 }
 
-info_msg() {
-
-    [[ "$quiet" = 1 ]] && return
-
-    local green='\033[1;32m'
-    local nocolor='\033[0m'
-
-    echo -e "\n${green}${1:-}${nocolor}"
-
-}
-
-inplace_msg() {
-
-    local msg="${1:-}"
-    local max_len
-    local msg_left
-    local msg_right
-
-    # replace newline characters with escaped representation for single line display
-    # msg="${msg//$'\n'/\\\\n}"
-
-    # for now replace inplace message new lines with a single space
-    msg="${msg//$'\n'/ }"
-    clear_line
-
-    if [ ${#msg} -gt $term_width ]; then
-        max_len=$((term_width - 5))
-        msg_left=$((max_len / 2))
-        msg_right=$((max_len - msg_left))
-        msg="${msg:0:$msg_left}...${msg: -$msg_right}"
-    fi
-
-    echo -ne "${msg}\r"
-
-}
-
 is_same_file() {
 
     local file="${1}"
@@ -288,6 +266,198 @@ is_same_file() {
     local target_hash=$(get_hash "$file")
 
     [[ "$source_hash" == "$target_hash" ]]
+
+}
+
+main() {
+
+    local head_regex='[0-9]{2,}'
+
+    [ -z "${1:-}" ] && show_help
+
+    command -v "sqlite3" > /dev/null || error_exit "The program \"sqlite3\" is required to store file hashess"
+    command -v "$hasher" > /dev/null || error_exit "The program \"$hasher\" is required to process file hashess"
+
+    while [ ${#} -gt 0 ] ; do
+        case "${1}" in
+
+            --compact|-c)
+                verbosity="$SHOW_FROM_COMPACT"
+                ;;
+            --debug|-d)
+                verbosity="$SHOW_FROM_DEBUG"
+                ;;
+            --dry-run)
+                dry_run=1
+                ;;
+            --flush-db|-f)
+                flush_db=1
+                ;;
+            --help|-h)
+                show_help
+                ;;
+            --keep-db|-k)
+                keep_db=1
+                ;;
+            --muted|-m)
+                verbosity="$SHOW_FROM_MUTED"
+                ;;
+            --no-color|-n)
+                no_color=1
+                ;;
+            -p)
+                partial=1
+                msg_info "using $head_size head size"
+                ;;
+            --partial)
+                partial=1
+                head_size="${2:-${head_size}}"
+                [[ ! $head_size =~ $head_regex ]] && error_exit "Invalid head size paramater value: $head_size"
+                msg_info "using $head_size head size"
+                shift 1
+                ;;
+            --progress|-p)
+                progress=1
+                ;;
+            --quiet|-q)
+                verbosity="$SHOW_FROM_QUIET"
+                ;;
+            --resume)
+                resume=1
+                ;;
+            --reuse-db|-r)
+                reuse_db=1
+                ;;
+            --verbose|-v)
+                verbosity="$SHOW_FROM_VERBOSE"
+                ;;
+            --*|-*)
+                error_exit "Unknown parameter: $1"
+                usage
+                ;;
+            *)
+                # Stop processing when we encounter a positional argument
+                break
+            ;;
+        esac
+        shift 1
+    done
+
+    src="${1:-}"
+    dst="${2:-}"
+
+    [[ "$#" -ne 2 ]] && error_exit "Missing source and target arguments."
+
+    [[ ! -d "$src" || ! -r "$dst" ]] && error_exit "Source directory does not exist or is not readable!"
+    [[ ! -d "$dst" || ! -w "$dst" ]] && error_exit "Destination directory does not exist or is not writable!"
+    [[ ! -d "$tmp" || ! -w "$tmp" ]] && error_exit "Temp directory does not exist or is not writable!"
+
+    # prevent long inplace messages from cluttering the terminal window
+    term_width=$(tput cols)
+
+    if [[ "$dry_run" = 1 ]]; then
+        keep_db=1
+        msg_warning "(Dry run mode - no filesystem changes. Preserving database after run.)"
+    fi
+
+    sync_target
+    cleanup_exit
+
+}
+
+msg() {
+
+    local msg="${1:-}"
+    local verbosity_level="${2:-SHOW_FROM_NORMAL}"
+    local type="${3:-normal}" # error, info, warning + normal, inplace
+    local max_len
+    local msg_left
+    local msg_right
+    local color=""
+    local color_reset=""
+    local post_msg=""
+    local flag_e="-e"
+    local flag_n=""
+
+    if [[ "$no_color" = 0 ]]; then
+
+        case "$type" in
+
+            "error")
+                # bright red
+                color="\033[1;31m"
+                ;;
+            "info")
+                # bright green
+                color="\033[1;32m"
+                ;;
+            "warning")
+                # bright yellow
+                color="\033[1;33m"
+                ;;
+        esac
+
+        [[ -n "$color" ]] && color_reset="\033[0m"
+
+    fi
+
+    # more verbosity than target disables inplace messages
+    if [[ ( "$type" = "inplace" && "$verbosity" -gt "$verbosity_level" ) ]]; then
+        type="normal"
+    fi
+
+    if [ "$type" = "inplace" ]; then
+
+        # replace newline characters with escaped representation for single line display
+        # msg="${msg//$'\n'/\\\\n}"
+
+        # for now replace inplace message new lines with a single space
+        msg="${msg//$'\n'/ }"
+        clear_line
+
+        if [ ${#msg} -gt $term_width ]; then
+            max_len=$((term_width - 5))
+            msg_left=$((max_len / 2))
+            msg_right=$((max_len - msg_left))
+            msg="${msg:0:$msg_left}...${msg: -$msg_right}"
+        fi
+
+        flag_n="-n"
+        post_msg="\r"
+
+    fi
+
+    [[ "$verbosity" -ge "$verbosity_level" ]] && echo $flag_n $flag_e "${color}${msg}${color_reset}${post_msg}"
+
+}
+
+msg_error(){
+
+    msg "$1" "${2:-$SHOW_FROM_QUIET}" "$MSG_TYPE_ERROR"
+
+}
+
+msg_info(){
+
+    msg "$1" "${2:-$SHOW_FROM_COMPACT}" "$MSG_TYPE_INFO"
+
+}
+
+msg_inplace(){
+
+    msg "$1" "${2:-$SHOW_FROM_COMPACT}" "$MSG_TYPE_INPLACE"
+
+}
+
+msg_normal(){
+
+    msg "$1" "${2:-$SHOW_FROM_NORMAL}" "$MSG_TYPE_NORMAL"
+
+}
+
+msg_warning(){
+
+    msg "$1" "${2:-$SHOW_FROM_COMPACT}" "$MSG_TYPE_WARNING"
 
 }
 
@@ -305,25 +475,18 @@ rename_existing_target() {
     done
 
     # unique filename, now rename...
-    print_msg "Renaming existing target with different content: $file -> $target"
+    msg_normal "Renaming existing target with different content: $file -> $target"
 
     # Rename and update only on success
     [[ "$dry_run" = 0 ]] && mv "$file" "$target" && update_path "$file" "$target"
 
 }
 
-update_path() {
-
-    local old_path="$1"
-    local new_path="$2"
-
-    db_query "UPDATE files SET path='${new_path//\'/\'\'}', used=1  WHERE path='${old_path//\'/\'\'}';"
-
-}
 
 show_help() {
 
-    echo "presync version $VERSION Copyright (c) Francisco Gonzalez
+    echo "presync version $VERSION Copyright (c) 2025 Francisco Gonzalez
+BSD-2 license
 
 presync renames files in a backup to match a reorganized source folder enabling
 efficient synchronization with rsync without unnecessary file copying.
@@ -331,15 +494,18 @@ efficient synchronization with rsync without unnecessary file copying.
 Usage: presync [OPTION]... SRC DEST
 
 Options
+--compact, -c    show less text output and use inplace progress messages
 --debug, -d      dumps database of targets before / after processing
 --dry-run        trial run without file changes (implies --keep-db)
 --flush-db, -f   remove any existing db without asking
 --help, -h       show this help
 --keep-db, -k    don't delete database after running (ignores --flush-db)
--p               same as --partial $head_size
+--muted, -m      don't output any text
+--no-color       print all messages without color
+-P               same as --partial $head_size
 --partial SIZE   calc checksums using at most N kilobytes from file
---progress       show progress of total files
---quiet, -q      show less text output and use inplace progress messages
+--progress, -p   show progress of total files
+--quiet, -q      show only inplace progress messages
 --resume         resume from last record in database (implies --reuse-db)
 --reuse-db, -r   use an existing database of targets without asking
 --verbose, -v    increase verbosity
@@ -378,8 +544,6 @@ synchronize move collection on slow USB drive with huge files:
 
 }
 
-
-
 sync_target() {
 
     local target
@@ -387,6 +551,7 @@ sync_target() {
     local files
     local total=0
     local progress_msg=""
+    local percent=""
 
     # stats: total files in source, total files in dest, total renamed files
     #        total identcal files in same path, total files in source not in target
@@ -398,35 +563,40 @@ sync_target() {
         collect_target_hashes
     fi
 
-    if [[ "$debug" = 1 ]]; then
-        notice_msg "\nList of collected target hashes before processing: (id, hash, path, used)"
-        db_query "select * from files;"
+    if [[ "$verbosity" -ge "$SHOW_FROM_DEBUG" ]]; then
+        msg_info "\nList of collected target hashes before processing: (id, hash, path, used)"
+        msg_warning "$(db_query "select * from files;")\n"
     fi
 
     clear_line
 
-    # print message regardless of verbosity level
-    echo "Processing sources and presyncing..."
-    print_msg "(Only files in need of reloaction are shown below)"
+    msg "Processing sources and presyncing..." "$SHOW_FROM_COMPACT"
 
-    [[ "$progress" = 1 ]] && total=$(get_total_files "$dst")
+    if [[ "$verbosity" -lt "$SHOW_FROM_VERBOSE" ]]; then
+        msg "(Only files in need of reloaction are shown below)" "$SHOW_FROM_COMPACT"
+    fi
+
+    [[ "$progress" = 1 ]] && total=$(get_total_files "$src")
 
     while IFS= read -r -d '' file; do
 
         (( idx++ ))
 
-        [[ "$progress" = 1 ]] && progress_msg="[${idx}/${total}] "
+        if [[ "$progress" = 1 ]]; then
+            percent=$((idx * 100 / total))
+            progress_msg="[${idx}/${total} (${percent}%)] "
+        fi
 
-        [[ "$quiet" = 1 ]] && inplace_msg "${progress_msg}${file}"
-
-        # verbose mode, show all files
-        [[ "$verbose" = 1 ]] && info_msg "${progress_msg}${file}"
+        # show all files being processed from verbosity verbose and up
+        if [[ "$verbosity" -gt "$SHOW_FROM_NORMAL" ]]; then
+            msg_info "${progress_msg}${file}"
+        fi
 
         hash=$(get_hash_from_file "$file")
         target="${file/#$src/$dst}"
 
         if [[ -z "$hash" ]]; then
-            error_msg "Error: cannot generate checksum for file: $file"
+            msg_error "Error: cannot generate checksum for file: $file"
             continue
         fi
 
@@ -437,8 +607,11 @@ sync_target() {
         # file exists in another path?
         if [[ -n "$existing_target" ]]; then
 
-            # normal mode, show only files being processed
-            [[ "$verbose" = 0 ]] && info_msg "${progress_msg}${file}"
+            case "$verbosity" in
+                "$SHOW_FROM_NORMAL") msg_info "${progress_msg}${file}" ;;
+                "$SHOW_FROM_COMPACT") msg_inplace "${progress_msg}${file}" ;;
+                "$SHOW_FROM_QUIET") [[ "$progress" = 1 ]] && msg_inplace "Collecting source hashes and processing: ${idx}/${total} (${percent}%)" "$SHOW_FROM_QUIET" ;;
+            esac
 
             # Rename existing target with different content since we have a candidate to take its place.
             [[ -f "$target" ]] && rename_existing_target "$hash" "$target"
@@ -447,17 +620,17 @@ sync_target() {
             [[ "$dry_run" = 0 ]] && [[ ! -d "${target%/*}" ]] && mkdir -p "${target%/*}"
 
             if [ -f "$target" ]; then
-                [[ "$dry_run" = 0 ]] && echo "Error: target file with different content already exists! May not have permission to move conflicting file:"
+                [[ "$dry_run" = 0 ]] && msg_error "Error: target file with different content already exists! May not have permission to move conflicting file: $target"
             else
                 # move existing_target to $target
-                print_msg "Moving: $existing_target -> $target"
+                msg_normal "Moving: $existing_target -> $target"
 
                 if [ "$dry_run" = 0 ] && mv "$existing_target" "$target"; then
                     # update database entry so we don't create orphans
                     update_path "$existing_target" "$target"
                 else
-                    # error_msg could be skipped on dry_run
-                    [[ "$dry_run" = 0 ]] && error_msg "Error: cannot move file!"
+                    # msg_error could be skipped on dry_run
+                    [[ "$dry_run" = 0 ]] && msg_error "Error: cannot move file!"
                 fi
 
             fi
@@ -466,117 +639,22 @@ sync_target() {
 
     done < <(find "$src" -type f -print0)
 
-    if [[ "$debug" = 1 ]]; then
-        notice_msg "\nList of collected target hashes after processing: (id, hash, path, used)"
-        db_query "select * from files;"
+    if [[ "$verbosity" -ge "$SHOW_FROM_DEBUG" ]]; then
+        msg_info "\nList of collected target hashes after processing: (id, hash, path, used)"
+        msg_warning "$(db_query "select * from files;")\n"
     fi
 
     clear_line
-    echo "Done!"
+    msg "Done!" "$SHOW_FROM_COMPACT"
 
 }
 
-main() {
+update_path() {
 
-    local head_regex='[0-9]{2,}'
+    local old_path="$1"
+    local new_path="$2"
 
-    [ -z "${1:-}" ] && show_help
-
-    command -v "sqlite3" > /dev/null || error_exit "The program \"sqlite3\" is required to store file hashess"
-    command -v "$hasher" > /dev/null || error_exit "The program \"$hasher\" is required to process file hashess"
-
-    while [ ${#} -gt 0 ] ; do
-        case "${1}" in
-
-            --debug|-d)
-                debug=1
-                ;;
-            --dry-run)
-                dry_run=1
-                ;;
-            --flush-db|-f)
-                flush_db=1
-                ;;
-            --help|-h)
-                show_help
-                ;;
-            --keep-db|-k)
-                keep_db=1
-                ;;
-            -p)
-                partial=1
-                info_msg "using $head_size head size"
-                ;;
-            --partial)
-                partial=1
-                head_size="${2:-${head_size}}"
-                [[ ! $head_size =~ $head_regex ]] && error_exit "Invalid head size paramater value: $head_size"
-                info_msg "using $head_size head size"
-                shift 1
-                ;;
-            --progress)
-                progress=1
-                ;;
-            --quiet|-q)
-                quiet=1
-                ;;
-            --resume)
-                resume=1
-                ;;
-            --reuse-db|-r)
-                reuse_db=1
-                ;;
-            --verbose|-v)
-                verbose=1
-                ;;
-            # Catch any unknown argument and stop processing
-            --*|-*)
-                error_exit "Unknown parameter: $1"
-                usage
-                ;;
-            *)
-                # Stop processing when we encounter a positional argument
-                break
-            ;;
-        esac
-        shift 1
-    done
-
-    src="${1:-}"
-    dst="${2:-}"
-
-    [[ "$#" -ne 2 ]] && error_exit "Missing source and target arguments."
-
-    [[ ! -d "$src" || ! -r "$dst" ]] && error_exit "Source directory does not exist or is not readable!"
-    [[ ! -d "$dst" || ! -w "$dst" ]] && error_exit "Destination directory does not exist or is not writable!"
-    [[ ! -d "$tmp" || ! -w "$tmp" ]] && error_exit "Temp directory does not exist or is not writable!"
-
-    # prevent long inplace messages from cluttering the terminal window
-    term_width=$(tput cols)
-
-    if [[ "$dry_run" = 1 ]]; then
-        keep_db=1
-        notice_msg "(Dry run mode - no filesystem changes. Preserving database after run.)"
-    fi
-
-    sync_target
-    cleanup_exit
-
-}
-
-notice_msg() {
-
-    local yellow='\033[1;93m'
-    local nocolor='\033[0m'
-
-    echo -e "${yellow}${1:-}${nocolor}"
-
-}
-
-print_msg() {
-
-    [[ "$quiet" = 1 ]] && return
-    echo -e "${1:-}"
+    db_query "UPDATE files SET path='${new_path//\'/\'\'}', used=1  WHERE path='${old_path//\'/\'\'}';"
 
 }
 
